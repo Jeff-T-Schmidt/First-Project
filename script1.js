84b23675d80483f9e52c1f5dfc57df4b7b3d3e60
Seval Cakir <sevalc@live.com>
2022-04-12 11:19:05 -0700

    for loop added to random cocktail

--- script1.js
+++ script1.js
@@ -1,39 +1,41 @@
-var requestRandomURL = 'https://www.thecocktaildb.com/api/json/v1/1/random.php'
-var randomBtn = document.getElementById("btn-random")
+var requestRandomURL = "https://www.thecocktaildb.com/api/json/v1/1/random.php";
+var randomBtn = document.getElementById("btn-random");
 
 function fetchRandom() {
-fetch(requestRandomURL)
+  fetch(requestRandomURL)
     .then(function (response) {
       return response.json();
     })
     .then(function (readableData) {
-      console.log(readableData)
-     
+      console.log(readableData);
+
       var cocktailName = document.getElementById("drinkName");
       cocktailName.textContent = readableData.drinks[0].strDrink;
-      
-      var cocktailmeasure1 = readableData.drinks[0].strMeasure1
-      var cocktailINgredients1 = readableData.drinks[0].strIngredient1;
-      document.getElementById("drinkIngredients1").textContent =
-        "Ingredients: " + cocktailINgredients1 + " " + cocktailmeasure1;
 
-        var cocktailmeasure2 = readableData.drinks[0].strMeasure2
-        var cocktailINgredients2 = readableData.drinks[0].strIngredient2;
-      document.getElementById("drinkIngredients2").textContent = cocktailINgredients2 + " " + cocktailmeasure2;
-
-      var cocktailmeasure3 = readableData.drinks[0].strMeasure3
-      var cocktailINgredients3 = readableData.drinks[0].strIngredient3;
-    document.getElementById("drinkIngredients3").textContent = cocktailINgredients3 + " " + cocktailmeasure3;
-
-    var cocktailmeasure4 = readableData.drinks[0].strMeasure4
-      var cocktailINgredients4 = readableData.drinks[0].strIngredient4;
-    document.getElementById("drinkIngredients4").textContent = cocktailINgredients4 + " " + cocktailmeasure4;
-
+      var count = 0;
+      for (var i = 1; i <= 15; i++) {
+        var fieldName = "strIngredient" + i;
+        var ingredient = readableData.drinks[0][fieldName];
+        if (ingredient === null) break;
+        
+        count++;
+      }
+      for (var i = 1; i <= count; i++) {
+        console.log(count);
+        var fieldName = "strIngredient" + i;
+        var ingredient = readableData.drinks[0][fieldName];
+        var fieldName2 = "strMeasure" + i;
+        var measure = readableData.drinks[0][fieldName2];
+        var ingredient = readableData.drinks[0][fieldName];
+        if (measure === null) break;
+        document.getElementById("drinkIngredients" + i).textContent =
+          ingredient + " " + measure;
+      }
+     
       var cocktailInstructions = document.getElementById("drinkInstructions");
       cocktailInstructions.textContent = readableData.drinks[0].strInstructions;
-
-    })
-};
+    });
+}
 fetchRandom();
 
-randomBtn.addEventListener('click', fetchRandom)
+randomBtn.addEventListener("click", fetchRandom);
